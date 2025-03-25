890b78b31873cdd09d635ae99b8c90ea69890a66
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2025-03-25 22:29:45 +1100

    For our project learn to drive after configuring the front end we are also creating the setup for the backend. In this commit There are few changing that have been made in which we hvae created a struture where frontend sits in our initial folder learn2 and learn2b is our new folder that has all the backends both of these folder will be under React. Furthermore, IN this commit we have succesfully connected to our data base moongoose and setup a server. Now next we will be testing some function through postman

---
+++ React/learn2b/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js'; 
+import Product from './models/product.model.js';
+
+dotenv.config();
+
+const app = express();
+
+app.post('/products', async (req, res) => {
+  const product = req.body;
+  
+  if(!product.name || !product.price || !product.image){
+    return res.status(400).json({success:false, message: 'Please provide all fields'});
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+      await newProduct.save();
+      res.status(201).json({success:true, data: newProduct});
+  } catch(error) {
+      console.error("Error in Create Product", error.message);
+      res.status(500).json({success:false, message: 'Server Error'});
+  }
+});
+
+
+
+app.listen(5000, () => {
+    connectDB();
+  console.log('Server is running on http://localhost:5000/hello');
+});
+
+//
