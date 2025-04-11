305ff321996f586ca5eb56708e1f0a541fca9574
jsiprsnt <jsiprsnt@gmail.com>
2025-04-11 14:09:18 +1000

    Hrllo

--- React/learn2/src/App.jsx
+++ React/learn2/src/App.jsx
@@ -7,6 +7,7 @@ import Register from './pages/Login-Registration/register';
 import InstructorPortal from './pages/InstructorPages/instructorPortal';
 import InstructorMenu from './pages/InstructorPages/instructorMenu';
 import InstructorProfile from './pages/InstructorPages/instructorProfile';
+import LessonReview from './pages/InstructorPages/lessonReview.jsx';
 
 import ManageStudents from './pages/InstructorPages/manageStudents';
 import DatePicker from './pages/studentPortal/datePicker.jsx';
@@ -19,11 +20,12 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* instructor page */}
+        {/* instructor pages */}
         <Route path="/instructorPortal" element={<InstructorPortal />} />
         <Route path="/instructorMenu" element={<InstructorMenu />} />
         <Route path="/instructorProfile" element={<InstructorProfile />} />
         <Route path="/manageStudents" element={<ManageStudents />} />
+        <Route path="/lessonReview" element={<LessonReview />} />
         <Route path="/studentPortal" element={<StudentPortal />} />
         <Route path="/datePicker/:instructorId" element={<DatePicker />}/>
         <Route path="/paymentForm" element={<PaymentForm />} />
